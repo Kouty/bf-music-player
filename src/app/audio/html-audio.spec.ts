e873dce400aecdd10c2336c3c84a1430d7f17caf
Andrea Koutifaris <kouty79@gmail.com>
2018-09-29 16:04:01 +0200

    Adds onTimeUpdate Observable to HtmlAudio

--- src/app/audio/html-audio.spec.ts
+++ src/app/audio/html-audio.spec.ts
@@ -29,18 +29,21 @@ describe('AudioTrack', () => {
   });
 
   describe('ontimeupdate ', () => {
-
     it('should register to ontimeupdate', () => {
       expect(audioElementMock.ontimeupdate).not.toBe(undefined);
     });
 
-    // it('should expose timeupdate event as an Observable', () => {
-    // audio.onTimeUpdate.subscribe((videoTime) => {
-    //
-    // });
-    // });
+    it('should expose timeupdate event as an Observable', () => {
+      let updateTime = 0;
+      const expectedTime = 12;
+      audio.onTimeUpdate().subscribe(videoTime => {
+        updateTime = videoTime;
+      });
 
+      audioElementMock.currentTime = expectedTime;
+      audioElementMock.ontimeupdate(<Event>{});
+
+      expect(updateTime).toBe(expectedTime);
+    });
   });
-
-
 });
--- src/app/audio/html-audio.ts
+++ src/app/audio/html-audio.ts
@@ -1,7 +1,11 @@
+import { Observable, Subject } from 'rxjs';
+
 export class HtmlAudio {
+  private timeSubject = new Subject<number>();
+
   constructor(private element: HTMLAudioElement) {
     this.element.ontimeupdate = () => {
-
+      this.timeSubject.next(this.element.currentTime);
     };
   }
 
@@ -11,5 +15,9 @@ export class HtmlAudio {
 
   get src(): string {
     return this.element.src;
+  }
+
+  onTimeUpdate(): Observable<number> {
+    return this.timeSubject;
   }
 }
