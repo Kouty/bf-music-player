1225e96d23182427fe34832427312c9431bd6820
Andrea Koutifaris <kouty79@gmail.com>
2018-09-29 19:05:53 +0200

    Fixes tests after refactor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
-import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { Audio } from './audio/html-audio';
+import { Audio } from './audio/audio';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -8,8 +7,9 @@ describe('AppComponent', () => {
 
   beforeEach(() => {
     audioMock = {
-      src: ''
-
+      src: '',
+      onPause: null,
+      onTimeUpdate: null
     };
     component = new AppComponent({
       createAudio: () => audioMock
--- src/app/audio/audio-provider.ts
+++ src/app/audio/audio-provider.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Audio } from './audio';
 import { HtmlAudio } from './html-audio';
 
 @Injectable()
 export class AudioProvider {
-  createAudio(): HtmlAudio {
+  createAudio(): Audio {
     return new HtmlAudio(document.createElement('audio'));
   }
 }
