c5225bf773a059b8e559656bfca8004484c55a6c
Andrea Koutifaris <kouty79@gmail.com>
2018-09-29 17:29:00 +0200

    Adds songs-queue to app.component with 2 songs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SongsQueueItem } from './songs-queue/songs-queue-item';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  queue: SongsQueueItem[] = [
+    {
+      src: 'https://www.bensound.com/bensound-music/bensound-ukulele.mp3',
+      title: 'Ukulele',
+      artist: 'Benjamin TISSOT',
+      album: 'Bensound'
+    },
+    {
+      src: 'https://www.bensound.com/bensound-music/bensound-summer.mp3',
+      title: 'Summer',
+      artist: 'Benjamin TISSOT',
+      album: 'Bensound'
+    }
+  ];
 }
