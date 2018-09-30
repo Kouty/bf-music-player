5eb919033e0d9af3367a28676e827e4e843996dd
Andrea Koutifaris <kouty79@gmail.com>
2018-09-30 19:53:03 +0200

    Changes ui

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import 'hammerjs';
+import 'normalize.css';
 import * as TWEEN from '@tweenjs/tween.js';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
