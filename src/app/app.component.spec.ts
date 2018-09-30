506358108d4451e493348fddb67bb6c4623e1b6a
Andrea Koutifaris <kouty79@gmail.com>
2018-09-30 09:09:53 +0200

    Refactores app.component.spec to remove ngOnInit duplication

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,34 +18,22 @@ describe('AppComponent', () => {
     component = new AppComponent({
       createAudio: () => <Audio>audioMock
     });
-  });
-
-  it('should register to onPause during init phase', () => {
-    spyOn(audioMock.onPause, 'subscribe');
-
     component.ngOnInit();
-
-    expect(audioMock.onPause.subscribe).toHaveBeenCalled();
   });
 
   it('should update pause state when onPause emits values', () => {
-    component.ngOnInit();
-
     audioMock.onPause.next(false);
 
     expect(component.trackBarModel.paused).toBe(false);
   });
 
   it('should update currentTime when onTimeUpdate emits values', () => {
-    component.ngOnInit();
-
     audioMock.onTimeUpdate.next(10);
 
     expect(component.trackBarModel.currentTime).toBe(10);
   });
 
   it('should play current track', () => {
-    component.ngOnInit();
     component.playCurrentTrack();
 
     expect(audioMock.src).not.toBe('');
@@ -53,27 +41,25 @@ describe('AppComponent', () => {
   });
 
   it('should play the current track when the user click play', () => {
-    component.ngOnInit();
-
     component.onPlayPauseClick(true);
 
     expect(audioMock.play).toHaveBeenCalled();
   });
 
   it('should pause the current track when the user click pause', () => {
-    component.ngOnInit();
-
     component.onPlayPauseClick(false);
 
     expect(audioMock.pause).toHaveBeenCalled();
   });
 
   it('should update track-bar model duration on audio metadata loaded', () => {
-    component.ngOnInit();
-
     audioMock.duration = 31;
     audioMock.onLoadedMetadata.next();
 
     expect(component.trackBarModel.duration).toBe(audioMock.duration);
+  });
+
+  xit('should play the selected song when the user tap on a track from the queue', ()=>{
+
   });
 });
