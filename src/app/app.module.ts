bdad4e6d3a9d6b1b8d961d4fdbf46e669a41fd88
Andrea Koutifaris <kouty79@gmail.com>
2018-10-01 22:23:03 +0200

    Working on track bar UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatSliderModule, MatTableModule } from '@angular/material';
+import { MatSliderModule, MatTableModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,7 +10,7 @@ import { AudioProvider } from './audio/audio-provider';
 
 @NgModule({
   declarations: [AppComponent, SongsQueueComponent, TrackBarComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule, MatTableModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule, MatTableModule, MatButtonModule, MatIconModule],
   providers: [AudioProvider],
   bootstrap: [AppComponent]
 })
