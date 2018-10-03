158e376eec55355cf0d34156638dadbdecb28ae4
Kouty <kouty79@gmail.com>
2018-10-03 17:49:37 +0200

    Adds images to the tracks list

--- src/app/track-data.ts
+++ src/app/track-data.ts
@@ -18,54 +18,63 @@ export const TRACKS: TrackData[] = [
     src: 'https://www.bensound.com/bensound-music/bensound-summer.mp3',
     title: 'Summer',
     artist: 'Benjamin TISSOT',
-    album: 'Bensound'
+    album: 'Bensound',
+    img: 'https://www.bensound.com/bensound-img/summer.jpg'
   },
   {
     src: 'https://www.bensound.com/bensound-music/bensound-anewbeginning.mp3',
     title: 'A new beginning',
     artist: 'Benjamin TISSOT',
-    album: 'Bensound'
+    album: 'Bensound',
+    img: 'https://www.bensound.com/bensound-img/anewbeginning.jpg'
   },
   {
     src: 'https://www.bensound.com/bensound-music/bensound-creativeminds.mp3',
     title: 'Creative minds',
     artist: 'Benjamin TISSOT',
-    album: 'Bensound'
+    album: 'Bensound',
+    img: 'https://www.bensound.com/bensound-img/creativeminds.jpg'
   },
   {
     src: 'https://www.bensound.com/bensound-music/bensound-jazzyfrenchy.mp3',
     title: 'Jazzy Frenchy',
     artist: 'Benjamin TISSOT',
-    album: 'Bensound'
+    album: 'Bensound',
+    img: 'https://www.bensound.com/bensound-img/jazzyfrenchy.jpg'
   },
   {
     src: 'https://www.bensound.com/bensound-music/bensound-littleidea.mp3',
     title: 'Little idea',
     artist: 'Benjamin TISSOT',
-    album: 'Bensound'
+    album: 'Bensound',
+    img: 'https://www.bensound.com/bensound-img/littleidea.jpg'
   },
   {
     src: 'https://www.bensound.com/bensound-music/bensound-happyrock.mp3',
     title: 'Happy rock',
     artist: 'Benjamin TISSOT',
-    album: 'Bensound'
+    album: 'Bensound',
+    img: 'https://www.bensound.com/bensound-img/happyrock.jpg'
   },
   {
     src: 'https://www.bensound.com/bensound-music/bensound-cute.mp3',
     title: 'Cute',
     artist: 'Benjamin TISSOT',
-    album: 'Bensound'
+    album: 'Bensound',
+    img: 'https://www.bensound.com/bensound-img/cute.jpg'
   },
   {
     src: 'https://www.bensound.com/bensound-music/bensound-hey.mp3',
     title: 'Hey',
     artist: 'Benjamin TISSOT',
-    album: 'Bensound'
+    album: 'Bensound',
+    img: 'https://www.bensound.com/bensound-img/hey.jpg'
   },
   {
     src: 'https://www.bensound.com/bensound-music/bensound-acousticbreeze.mp3',
     title: 'Acoustic breeze',
     artist: 'Benjamin TISSOT',
-    album: 'Bensound'
+    album: 'Bensound',
+    img: 'https://www.bensound.com/bensound-img/acousticbreeze.jpg'
   }
 ];
